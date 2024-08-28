d350f80c4dd98453c5ca2ca8435fd37e8af37bb2
Alder998 <davide.ontano@yahoo.com>
2024-08-28 11:52:37 +0200

    Commit 2.1: Text adaptation for fine-tuning

--- Model/fineTuningGeneral.py
+++ Model/fineTuningGeneral.py
@@ -4,6 +4,7 @@ import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from datasets import load_dataset
 from fineTuningDatasetGenerator import fineTuning_Utils as utils
+from transformers import Trainer, TrainingArguments
 
 class fineTuningGeneral:
 
@@ -16,7 +17,29 @@ class fineTuningGeneral:
         model = GPT2LMHeadModel.from_pretrained(baseModel)
 
         # Prepare the data that you want your Model to be trained on
-
         preparedText = utils.fineTuning_utils().getFineTuningContinuousTextFromDataset(datasetPandas)
+
+        # Tokeinze the dataset
+        inputs = tokenizer(preparedText, return_tensors='pt', max_length=1024, truncation=True)
+
+        # Model fine-Tuning
+        training_args = TrainingArguments(
+            output_dir='./results',
+            overwrite_output_dir=True,
+            num_train_epochs=10,
+            per_device_train_batch_size=1,
+            save_steps=500,
+            save_total_limit=2,
+            logging_dir='./logs',
+        )
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=inputs,
+        )
+        trainer.train()
+
+        model.save_pretrained('./fine_tuned_gpt2')
+        tokenizer.save_pretrained('./fine_tuned_gpt2')
 
         return preparedText
--- fineTuningDatasetGenerator/fineTuning_Utils.py
+++ fineTuningDatasetGenerator/fineTuning_Utils.py
@@ -1,5 +1,7 @@
 # General file for multi-usage functions for processing text data for fine-tuning
 
+import pandas as pd
+
 class fineTuning_utils:
 
     def __init__(self):
@@ -7,7 +9,22 @@ class fineTuning_utils:
 
     def getFineTuningContinuousTextFromDataset (self, datasetPandas):
 
-        # Prepare the dataset in JSON format, before converting it into
+        # Prepare the dataset in JSON format, before converting it into continuous text
+        dictList = list()
+        for singleRow in range(len(datasetPandas[datasetPandas.columns[0]])):
+            singleRowData = pd.DataFrame(datasetPandas.iloc[singleRow]).transpose().reset_index(drop=True)
+            dictList.append(singleRowData.iloc[0].to_dict())
 
+        # Then, the file needs to be converted into Continuous Text format, with delimiters to help the model understand
+        # what is the topic of the fine-tuning Dataset, therefore, the settings must be <COLUMN_NAME> text </COLUMN_NAME>
+        # and so on. The desired file has to be a long string file with all the fine-tuning information
 
-        return datasetPandas
+        totalStringList = list()
+        for singleDict in dictList:
+            for key, value in singleDict.items():
+                # Compose the long Text for any single Item
+                continuousTextSingleString = ('<' + key.upper() + '> ' + str(value) + ' </' + key.upper() + '> ')
+                totalStringList.append(continuousTextSingleString)
+        totalStringList = ' '.join(totalStringList)
+
+        return totalStringList
